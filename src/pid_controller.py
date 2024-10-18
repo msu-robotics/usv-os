46e523624d9ffb767c4e61c9844e97775f3956af
Photon94 <299792458.photon.94@gmail.com>
2024-10-18 13:30:52 +0300

    fix pid calculation error

--- src/pid_controller.py
+++ src/pid_controller.py
@@ -2,15 +2,6 @@
 
 class PIDController:
     def __init__(self, Kp, Ki, Kd, setpoint=0, output_limits=(-100, 100)):
-        """
-        Инициализация PID-регулятора.
-
-        :param Kp: Пропорциональный коэффициент
-        :param Ki: Интегральный коэффициент
-        :param Kd: Дифференциальный коэффициент
-        :param setpoint: Желаемое значение (уставка)
-        :param output_limits: Кортеж с минимальным и максимальным значением выходного сигнала
-        """
         self.Kp = Kp
         self.Ki = Ki
         self.Kd = Kd
@@ -21,14 +12,8 @@ class PIDController:
         self._output_limits = output_limits
 
     def update(self, measurement, dt):
-        """
-        Обновление PID-регулятора на основе текущего измерения и времени.
-
-        :param measurement: Текущее значение измерения (например, текущий угол yaw)
-        :param dt: Время, прошедшее с последнего вызова (в секундах)
-        :return: Вычисленное управляющее воздействие
-        """
-        error = abs(self.setpoint) - abs(measurement)
+        # Вычисление угловой ошибки с учетом цикличности
+        error = (self.setpoint - measurement + 180) % 360 - 180
 
         # Пропорциональная составляющая
         P = self.Kp * error
